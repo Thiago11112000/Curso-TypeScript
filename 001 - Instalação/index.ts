bb80ccf9ca24fbe0843a306a538cd23cd4dcfb94
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-23 20:56:10 -0300

    Instalando

---
+++ 001 - Instalação/index.ts
@@ -0,0 +1,16 @@
+// Typescript
+// npm install -g typescript
+// tsc --init
+
+// Plugin: Code Runner
+// npm i -g ts-node
+// ctrl + alt + n
+
+
+console.log("npm install -g TypeScript");
+
+const pedido = (pedido:number) =>{
+    return `Deu bom o seu pedido ${pedido}`
+};
+
+console.log(pedido(123))
