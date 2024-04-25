e280dab4b7a66acb72bdf9dde9b08a24a9a8da7d
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-25 16:40:28 -0300

    Enum

---
+++ 002 - Tipos/009 - Enum/index.ts
@@ -0,0 +1,12 @@
+enum Mes{
+  JAN = "Janeiro",
+  FEV = "Fevereiro",
+  MAR = "Março",
+}
+const pessoa: {nome: string, mesAniversario: string} = {
+ nome: "Thiago",
+ mesAniversario: Mes.MAR
+}
+if(pessoa.mesAniversario === Mes.MAR){
+    console.log(pessoa)
+}
