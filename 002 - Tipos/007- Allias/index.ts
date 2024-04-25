e40e82974bbcbd9d5b4e5849adb953cf09c4c76a
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-25 16:03:41 -0300

    Allias

---
+++ 002 - Tipos/007- Allias/index.ts
@@ -0,0 +1,13 @@
+
+ type alphanumeric = string | number;
+
+let dados: alphanumeric;
+let dados2: alphanumeric;
+
+dados = 1 
+dados = "Thiago"
+dados = true
+
+dados2 = 1 
+dados2 = "Thiago"
+dados2 = true
