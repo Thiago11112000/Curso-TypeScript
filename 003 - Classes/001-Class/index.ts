757f9a61253d93d93bb279e9f3f05d4dd8a1ae17
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-26 20:20:55 -0300

    Class

---
+++ 003 - Classes/001-Class/index.ts
@@ -0,0 +1,34 @@
+/*
+  Uma classe TypeScript é uma estrutura de programação orientada a objetos 
+  que possui um conjunto de propriedades e métodos. 
+
+  A classe é um modelo para criar objetos, permitindo que você defina 
+  uma estrutura com propriedades e comportamentos.
+*/
+
+class Pessoa {
+  nome: string = "" ;
+  idade: number = 0;
+
+  constructor(nome: string, idade:number){
+   this.nome = nome;
+   this.idade = idade;
+  }
+  
+}
+ const pessoa1 = new Pessoa("Thiago Martins", 23)
+ const pessoa2 = new Pessoa("Caio Jorge", 24)
+ console.log(pessoa1.nome);
+ console.log(pessoa1.idade);
+
+ console.log(pessoa2.nome);
+ console.log(pessoa2.idade);
+
+ 
+
+
+
+
+
+
+
