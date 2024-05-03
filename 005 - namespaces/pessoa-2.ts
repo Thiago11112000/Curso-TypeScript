9b2845f87ae2474173a2e4c287d04e7ce9833057
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-05-03 11:33:47 -0300

    Intrudoção namespaces

---
+++ 005 - namespaces/pessoa-2.ts
@@ -0,0 +1,12 @@
+/*
+ Os Namespaces são uma forma específica do TypeScript para organizar código. 
+ Namespaces são simplesmente objetos JavaScript nomeados no namespace global. 
+ Isso torna os namespaces uma construção muito simples de usar.
+*/
+
+import { Pessoa1} from "./pessoa-1";
+
+console.log(Pessoa1.nome);
+console.log(Pessoa1.calc());
+
+
